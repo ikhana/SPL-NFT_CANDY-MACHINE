dbaa92d9237e4d66e967a5ce423ad825e038a68c
ikhana <Inaa.eth@gmail.com>
2024-02-05 19:00:29 +0500

    mint nft method in program

--- programs/first-nft/src/lib.rs
+++ programs/first-nft/src/lib.rs
@@ -1,7 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{Mint, Token, TokenAccount},
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
 };
 declare_id!("FYJwg6zgrXCqMuEM5Nak8EMdCXyfDirYSkcvcYJZaxBT");
 
@@ -9,11 +9,21 @@ declare_id!("FYJwg6zgrXCqMuEM5Nak8EMdCXyfDirYSkcvcYJZaxBT");
 pub mod first_nft {
     use super::*;
 
-    pub fn initialize(_ctx: Context<InitNFT>) -> Result<()> {
+    pub fn init_nft(ctx: Context<InitNFT>) -> Result<()> {
+        // create mint account
+        let cpi_context = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            MintTo {
+                mint: ctx.accounts.mint.to_account_info(),
+                to: ctx.accounts.associated_token_account.to_account_info(),
+                authority: ctx.accounts.signer.to_account_info(),
+            },
+        );
+
+        mint_to(cpi_context, 1)?;
         Ok(())
     }
 }
-
 #[derive(Accounts)]
 
 pub struct InitNFT<'info> {
